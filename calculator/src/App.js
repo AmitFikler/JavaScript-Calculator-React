5bdf9b4791754c566562c93ac8d103a6a37bff35
AmitFikler <amitfikler@gmail.com>
2021-11-19 15:57:16 +0200

    add callback to pressOnEquels

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -9,42 +9,77 @@ class App extends React.Component {
     super(props);
     this.state = {
       answer: 0,
-      calculte: {
-        firstNumber: undefined,
-        sign: undefined,
-        secondNumber: undefined,
-      },
+      firstNumber: undefined,
+      secondNumber: undefined,
+      sign: undefined,
     };
   }
   pressOnNumber = (number) => {
     if (this.state.answer === 0) {
-      this.setState({ answer: number });
+      if (number !== '.') {
+        this.setState({ answer: number });
+      } else {
+        return;
+      }
     } else {
       this.setState({ answer: this.state.answer + number });
     }
   };
   pressOnSign = (sign) => {
     this.setState({
-      calculte: {
-        firstNumber: this.state.answer,
-        secondNumber: undefined,
-        sign: sign,
-      },
+      firstNumber: this.state.answer,
+      sign: sign,
     });
     this.setState({ answer: 0 });
-    console.log(this.state.calculte, this.state.answer);
+  };
+  pressOnEquels = () => {
+    this.setState({ secondNumber: this.state.answer }, () => {
+      let answer;
+      switch (this.state.sign) {
+        case '+':
+          answer =
+            Number(this.state.firstNumber) + Number(this.state.secondNumber);
+          this.setState({ answer: answer });
+          break;
+        case '-':
+          answer =
+            Number(this.state.firstNumber) - Number(this.state.secondNumber);
+          this.setState({ answer: answer });
+          break;
+        case 'X':
+          answer =
+            Number(this.state.firstNumber) * Number(this.state.secondNumber);
+          this.setState({ answer: answer });
+          break;
+        case '/':
+          answer =
+            Number(this.state.firstNumber) / Number(this.state.secondNumber);
+          this.setState({ answer: answer });
+          break;
+        default:
+          break;
+      }
+    });
+  };
+  pressOnAC = () => {
+    this.setState({
+      answer: 0,
+      firstNumber: undefined,
+      sign: undefined,
+      secondNumber: undefined,
+    });
   };
   render() {
     return (
       <div className="parent">
         <div className="device"> </div>
         <Screen answer={this.state.answer} />
-        <Button className="AC" sign="AC" />
+        <Button className="AC" sign="AC" func={this.pressOnAC} />
         <Button className="div sign" sign="/" func={this.pressOnSign} />
         <Button className="mul sign" sign="X" func={this.pressOnSign} />
         <Button className="minus sign" sign="-" func={this.pressOnSign} />
         <Button className="plus sign" sign="+" func={this.pressOnSign} />
-        <Button className="equels" sign="=" func={this.pressOnSign} />
+        <Button className="equels" sign="=" func={this.pressOnEquels} />
         <Button className="one number" sign="1" func={this.pressOnNumber} />
         <Button className="two number" sign="2" func={this.pressOnNumber} />
         <Button className="three number" sign="3" func={this.pressOnNumber} />
@@ -55,7 +90,7 @@ class App extends React.Component {
         <Button className="eight number" sign="8" func={this.pressOnNumber} />
         <Button className="nine number" sign="9" func={this.pressOnNumber} />
         <Button className="zero number" sign="0" func={this.pressOnNumber} />
-        <Button className="dot" sign="." />
+        <Button className="dot" sign="." func={this.pressOnNumber} />
       </div>
     );
   }
